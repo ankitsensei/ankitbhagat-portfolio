a658461d19a23e3419928db786ec439a96e90b48
ankitsensei <main.ankitbhagat@gmail.com>
2025-03-05 23:51:32 +0530

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="h-screen flex flex-col justify-center mx-60">
+      <Navbar />
+    </div>
   )
 }
 
---
+++ src/components/Data.ts
@@ -0,0 +1,9 @@
+// Define the type for better type safety
+type Detail = [string, string, string];
+
+const details: Detail[] = [
+    ['Introduction', '2025', 'Hey there, I am Ankit Bhagat'],
+    ['Nonsense', '2028', 'Hey there, how are you?']
+];
+
+export default details;
