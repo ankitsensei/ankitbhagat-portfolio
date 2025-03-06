5fce1d5f41a5d857c49254d678e5bccb2b1c91be
ankitsensei <main.ankitbhagat@gmail.com>
2025-03-06 23:42:32 +0530

    tweaking with hover effect

--- src/App.tsx
+++ src/App.tsx
@@ -22,10 +22,11 @@ const App = () => {
       <div className='w-1/3'>
         <Navbar Name="DM Resharing" Year="2022" />
         <Navbar Name="Media Viewer" Year="2022" />
+        <Navbar Name="Telegram Planner" Year="2023" />
       </div>
       <div className='w-2/3'>
-        <h3>Ankit Bhagat</h3>
-        <h3>Just a developer at nowhere</h3>
+        <h3 className='text-2xl font-semibold bg-gradient-to-r from-purple-500 via-orange-500 to-pink-500 bg-clip-text text-transparent'>Ankit Bhagat,</h3>
+        <h3 className='text-2xl font-semibold'>Just a developer at nowhere</h3>
         <div>
           // Here Links will be there
         </div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import details from './Data';
 const Navbar = ({Name, Year}) => {
   return (
-    <div className='flex justify-between w-44 py-3'>
+    <div className='flex justify-between w-60 my-2 py-2 px-3 hover:text-lg hover:border-2 rounded-3xl'>
         <p>{Name}</p>
         <p>{Year}</p>
     </div>
