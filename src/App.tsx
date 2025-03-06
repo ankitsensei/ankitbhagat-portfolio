6fdd093b8db1503cd9889907901abe210a50ff35
ankitsensei <main.ankitbhagat@gmail.com>
2025-03-06 21:45:08 +0530

    corrected width of page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className="h-screen flex flex-col justify-center mx-60">
-      <div>
+    <div className="h-screen flex justify-around items-center mx-60">
+      <div className='w-1/3'>
         <Navbar Name="DM Resharing" Year="2022" />
         <Navbar Name="Media Viewer" Year="2022" />
       </div>
-      <div>
+      <div className='w-2/3'>
         <h3>Ankit Bhagat</h3>
         <h3>Just a developer at nowhere</h3>
         <div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import details from './Data';
 const Navbar = ({Name, Year}) => {
   return (
-    <div className='flex flex-col justify-between'>
+    <div className='flex justify-between w-44 py-3'>
         <p>{Name}</p>
         <p>{Year}</p>
     </div>
