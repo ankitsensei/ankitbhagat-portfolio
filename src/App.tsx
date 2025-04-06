0b22ee54ae790a00049c54ce065c4a4b5ae62442
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-06 14:37:38 +0530

    header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-black w-full h-screen text-white">
+      <div className='px-4 h-full flex flex-col pt-20'>
+        <div className='flex flex-col gap-7'>
+          <div>
+            <p>Ankit</p>
+            <p className='text-zinc-400'>Software Enginner</p>
+          </div>
+          <p className='text-zinc-300'>Break things and fixing them back. Software Enginner, Designer and lot more.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
