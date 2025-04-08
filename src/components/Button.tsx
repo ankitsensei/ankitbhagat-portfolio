f411b7c894d6691fe59705622e6b288ce2a80710
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-08 22:07:51 +0530

    enhance Button component to accept links and update Connect component with social media links

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,12 @@
 import { FaArrowRight } from "react-icons/fa"
-const Button = ({btnName}) => {
+const Button = ({btnName, link}) => {
   return (
     <div>
-        <button className="flex justify-center items-center gap-2 text-sm px-3 h-8 rounded-3xl bg-zinc-800">
+        <a href={link} target="#"><button className="flex justify-center items-center gap-2 text-sm px-3 h-8 rounded-3xl bg-zinc-800">
             {btnName}
             <FaArrowRight className="rotate-320 text-[10px]" />
-        </button>
+        </button></a>
+        
     </div>
   )
 }
--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -5,11 +5,11 @@ const Connect = () => {
         <h2 className="text-lg font-semibold">Connect</h2>
         <p className="text-sm text-zinc-300">Feel free to contact me <u>main.ankitbhagat@gmail.com</u></p>
         <div className="flex flex-wrap gap-2">
-            <Button btnName="Github"/>
-            <Button btnName="X"/>
-            <Button btnName="Linkedin"/>
-            <Button btnName="Instagram"/>
-            <Button btnName="Resume"/>
+            <Button btnName="Github" link="https://github.com/ankitsensei"/>
+            <Button btnName="X" link="https://x.com/webdevankit"/>
+            <Button btnName="Linkedin" link="https://www.linkedin.com/in/ankit-bhagat-139276241/"/>
+            <Button btnName="Instagram" link="https://www.instagram.com/ankitsensei/"/>
+            <Button btnName="Resume" link="https://www.instagram.com/ankitsensei/"/>
         </div>
     </div>
   )
