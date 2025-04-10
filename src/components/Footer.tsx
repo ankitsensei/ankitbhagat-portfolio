3625968060297142dc94921f59081c177b0be8b3
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-10 08:36:05 +0530

    added light, dark and system default icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faSun, faMoon, faDesktop } from '@fortawesome/free-solid-svg-icons'
 
 
 const Footer = () => {
   return (
-    <div className="mt-30 ">
+    <div className="mt-30">
         <hr className="text-zinc-700 mx-5"/>
-        <div className="w-full h-14 flex flex-col justify-center px-5">
+        <div className="w-full h-14 flex justify-between items-center px-5">
             <p className="text-[13px] text-zinc-400">@ankitbhagat</p>
-            <div>
-            <FontAwesomeIcon icon={faEnvelope} />
+            <div className="flex gap-3">
+              <FontAwesomeIcon icon={faSun} />
+              <FontAwesomeIcon icon={faMoon} />
+              <FontAwesomeIcon icon={faDesktop} />
             </div>
         </div>
     </div>
