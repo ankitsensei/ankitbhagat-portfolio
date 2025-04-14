1aa20c3ef6296fc40d3ce5d84158ead98d2a66d0
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-14 23:26:41 +0530

    semibold mail

--- src/components/Connect.tsx
+++ src/components/Connect.tsx
@@ -3,7 +3,8 @@ const Connect = () => {
   return (
     <div className='mt-10 px-4 flex flex-col gap-4'>
         <h2 className="text-lg font-semibold">Connect</h2>
-        <p className="text-sm text-zinc-300">Feel free to contact me <a href="mailto:main.ankitbhagat@gmail.com" className="underline">main.ankitbhagat@gmail.com</a></p>
+        <p className="text-sm text-zinc-300">Feel free to contact me <a href="mailto:main.ankitbhagat@gmail.com" 
+          className="underline font-semibold">main.ankitbhagat@gmail.com</a></p>
         <div className="flex flex-wrap gap-2">
             <Button btnName="Github" link="https://github.com/ankitsensei"/>
             <Button btnName="X" link="https://x.com/webdevankit"/>
