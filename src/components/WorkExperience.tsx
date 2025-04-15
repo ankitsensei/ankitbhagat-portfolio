d2365f3d4a6928825dbb5f2c603877f2cba6d048
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-15 23:42:27 +0530

    dark and light mode work experience

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -9,16 +9,16 @@ const WorkExperience: React.FC= () => {
                     <h1 className="text-lg">Work Experience</h1>
                     <Button btnName="Know more"/>
                 </div>
-                <div className="border-2 border-zinc-900 rounded-xl w-full m-2 p-2 flex flex-col gap-4">
+                <div className="border-2 border-zinc-100 dark:border-zinc-900 rounded-xl w-full m-2 p-2 flex flex-col gap-4">
                     <div>
                         <div className="flex justify-between ">
                             <h2>Frontend Web-Developer</h2>
-                            <p className="text-zinc-400">November 2024</p>
+                            <p className="text-zinc-600 dark:text-zinc-400">November 2024</p>
                         </div>
-                        <p className='text-zinc-400'>Hack United (US)</p>
+                        <p className='text-zinc-500 dark:text-zinc-400'>Hack United (US)</p>
                     </div>
-                    <ul className='text-zinc-400 flex flex-col gap-2 list-disc px-5'>
-                        <li><b className='underline'>Discord Server: Hack United</b> - A global community that organizes online <b className='text-white'>hackathons</b>, connecting developers, designers, and tech enthusiasts from around the world.</li>
+                    <ul className='text-zinc-600 dark:text-zinc-400 flex flex-col gap-2 list-disc px-5'>
+                        <li><b className='underline'>Discord Server: Hack United</b> - A global community that organizes online <b className='text-zinc-600 dark:text-white'>hackathons</b>, connecting developers, designers, and tech enthusiasts from around the world.</li>
 
                         <li><b className='underline'>Role: Web Developer & Manager</b> – I design, develop, and maintain the official websites for Hack United, ensuring smooth registration, event updates, and participant engagement.</li>
 
