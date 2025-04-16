d5f7a5a3244abb815198ac5b4a5e7ac1bf0d43a7
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-16 21:33:59 +0530

    changed dp to circle

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,10 +1,14 @@
-const Intro = () => {
+import { useState } from 'react';
+
+
+const Intro: React.FC = () => {
+  const [status, setStatus] = useState<string>("Software Enginner")
   return (
     <div>
         <div className='flex flex-col gap-7 px-4 '>
             <div>
                 <p className='text-lg'>Ankit</p>
-                <p className='text-zinc-600 dark:text-zinc-400'>Software Enginner</p>
+                <p className='text-zinc-600 dark:text-zinc-400'>{status}</p>
             </div>
         <p className='text-zinc-600 dark:text-zinc-300'>Break things and fixing them back. Software Enginner, Designer and lot more.</p>
     </div>
