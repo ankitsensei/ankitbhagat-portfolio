41c9e68924894e01db28d315d5ba71245a4b645b
ankitsensei <main.ankitbhagat@gmail.com>
2025-04-16 17:41:58 +0530

    dark mode, light mode, system default mode working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { motion } from 'motion/react'
+import { motion } from 'framer-motion'
+import { ThemeProvider } from './components/ThemeContext'
 
 import Intro from './components/Intro'
 import WorkExperience from './components/WorkExperience'
@@ -7,18 +8,19 @@ import Connect from "./components/Connect"
 import Footer from './components/Footer'
 
 const App = () => {
-
   return (
-    <motion.div className="bg-white dark:bg-black w-full h-full text-black dark:text-white">
-      <div className='h-full flex flex-col pt-20 lg:w-2/5 lg:mx-[30%]'>
-        <Intro/>
-        <WorkExperience />
-        <Projects />
-        <Connect />
-        <Footer />
-      </div>
-    </motion.div>
+    <ThemeProvider>
+      <motion.div className="bg-white dark:bg-black w-full h-full text-black dark:text-white">
+        <div className='h-full flex flex-col pt-20 lg:w-2/5 lg:mx-[30%]'>
+          <Intro />
+          <WorkExperience />
+          <Projects />
+          <Connect />
+          <Footer />
+        </div>
+      </motion.div>
+    </ThemeProvider>
   )
 }
 
-export default App 
+export default App
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,22 +1,23 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
 import { faSun, faMoon, faDesktop } from '@fortawesome/free-solid-svg-icons'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { useTheme } from './ThemeContext'
 
 const Footer = () => {
+  const { setTheme } = useTheme();
+
   return (
     <div className="mt-30">
-        <hr className="text-zinc-700 mx-5"/>
-        <div className="w-full h-14 flex justify-between items-center px-5">
-            <p className="text-[13px] text-zinc-600 dark:text-zinc-400">@ankitbhagat</p>
-            <div className="flex gap-3">
-              <FontAwesomeIcon icon={faSun} />
-              <FontAwesomeIcon icon={faMoon} />
-              <FontAwesomeIcon icon={faDesktop} />
-            </div>
+      <hr className="text-zinc-700 mx-5" />
+      <div className="w-full h-14 flex justify-between items-center px-5">
+        <p className="text-[13px] text-zinc-600 dark:text-zinc-400">@ankitbhagat</p>
+        <div className="flex gap-3">
+          <button onClick={() => setTheme("light")}><FontAwesomeIcon icon={faSun} /></button>
+          <button onClick={() => setTheme("dark")}><FontAwesomeIcon icon={faMoon} /></button>
+          <button onClick={() => setTheme("system")}><FontAwesomeIcon icon={faDesktop} /></button>
         </div>
+      </div>
     </div>
   )
 }
 
-export default Footer
+export default Footer;
