3908f2bd0c1dd1c48ca9598e51a85dbc0f64e92c
ankitsensei <main.ankitbhagat@gmail.com>
2025-09-02 22:44:40 +0530

    added another project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,14 +2,16 @@
 import ProjectComp from "./ProjectsComp"
 import FocusMode from "../assets/imgs/focusModeExtension.png"
 import MovieDB from "../assets/imgs/MovieDB.png"
+import BillSplitter from "../assets/imgs/Bill-splitter.png"
 const Projects = () => {
   return (
     <div className="mt-10 px-4  w-full h-full bg-white dark:bg-black">
-        <h1 className="text-lg font-medium">Projects</h1>
-        <div className="lg:flex gap-8">
-            <ProjectComp image={FocusMode} heading='Focus Mode' description='It is a chrome extension which block any sites you put in it. It really helps you to be productive without any distractions.' />
-            <ProjectComp image={MovieDB} heading='MovieDB' description="It is a movie database, where you can find a lot of movies. (Can't watch)"/>
-        </div>
+      <h1 className="text-lg font-medium">Projects</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+        <ProjectComp image={BillSplitter} heading='Bill Splitter' description='A bill Splitter Web App, which stores the history of bill splits among friends. It has two sections Unsettled and Settled which shows dues paid by friends or not. And other features too.' />
+        <ProjectComp image={FocusMode} heading='Focus Mode' description='It is a chrome extension which block any sites you put in it. It really helps you to be productive without any distractions.' />
+        <ProjectComp image={MovieDB} heading='MovieDB' description="It is a movie database, where you can find a lot of movies. (Can't watch)" />
+      </div>
 
     </div>
   )
