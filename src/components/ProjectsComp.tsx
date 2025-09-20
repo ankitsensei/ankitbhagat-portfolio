8ce2c2524d67dca9bd28cbd2932cc19e0c61190e
ankitsensei <main.ankitbhagat@gmail.com>
2025-09-20 11:17:51 +0530

    In projects section, Image preview feature added

--- src/components/ProjectsComp.tsx
+++ src/components/ProjectsComp.tsx
@@ -2,15 +2,17 @@ type ProjectProps = {
   image: string;
   heading: string;
   description: string;
+  onImageClick?: (image: string) => void;
 }
 
-const ProjectsComp = ({image, heading, description}: ProjectProps) => {
+const ProjectsComp = ({ image, heading, description, onImageClick }: ProjectProps) => {
 
-return (
+  return (
     <div className="flex flex-col mt-8 gap-1">
-        <img src={image} alt={heading || "Project image"} className="rounded-2xl border-2 border-zinc-800"/>
-        <h2 className="font-medium">{heading}</h2>
-        <p className="text-zinc-600 dark:text-zinc-400 text-md">{description}</p>
+      <img src={image} alt={heading || "Project image"} className="rounded-2xl border-2 border-zinc-800"
+        onClick={() => onImageClick && onImageClick(image)} />
+      <h2 className="font-medium">{heading}</h2>
+      <p className="text-zinc-600 dark:text-zinc-400 text-md">{description}</p>
     </div>
   )
 }
