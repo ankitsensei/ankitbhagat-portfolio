b5593906e91a7540a88dababde1bfe9726acec87
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2025-12-08 23:04:37 +0530

    changing the design: Added 'what i do'

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -8,11 +8,15 @@ const Intro: React.FC = () => {
         transition={{ duration: 0.8, ease: "easeOut" }}>
         <div className='flex flex-col gap-7 '>
             <div className="flex flex-col gap-2">
-                <p className='text-2xl font-roman'>Sup, I'm Ankit Bhagat 👋</p>
-                <p className=' text-xl'>Frontend Developer</p>
+                <p className='text-2xl font-roman text-white'>Sup, I'm Ankit Bhagat 👋</p>
+                <p className=' text-lg text-zinc-300'>Frontend Developer</p>
                 <p className="text-zinc-600 dark:text-zinc-400">I'm a Frontend Web Developer from Jharkhand, India, trying to make the internet a bit cooler one website at a time.</p>
             </div>
-        <p className='text-zinc-600 dark:text-zinc-300'>Break things and fixing them back. Software Enginner, Designer and lot more.</p>
+        <div className="flex flex-col gap-2 mt-5">
+          <h1 className="text-2xl">What I do?</h1>
+          <p className="text-zinc-400 text-md">Currently, I am a 2nd year B.Tech Student of Computer Science department. As a developer, I am more focused into Web Development.</p>
+          <p className="text-zinc-400 text-md">Apart from Programming and studies, I enjoy video editing, cooking, doing something new everyday and many more...</p>
+        </div>
     </div>
     </motion.div>
   )
