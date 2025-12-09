c1809e4113bdadfec9ae81608e9111f3e0588ff1
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2025-12-09 22:28:05 +0530

    Made a new folder pages... And added navbar items into it

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,10 @@ import { motion } from "motion/react"
 
 import Navbar from "./components/Navbar"
 import Intro from './components/Intro'
-// import WorkExperience from './components/WorkExperience'
-// import Projects from './components/Projects'
-// import Connect from "./components/Connect"
 import Footer from './components/Footer'
-// import TechIUse from "./components/TechIUse"
+import Home from './pages/Home'
 
 
-// import Resume from "./components/Resume"
 
 const App = () => {
   return (
@@ -18,14 +14,8 @@ const App = () => {
       <motion.div
         className=" bg-white dark:bg-black w-full h-full text-black dark:text-white">
         <div className='h-full flex flex-col pt-20 w-full max-w-2xl mx-auto font-geist'>
-            <Navbar />
-            <Intro />
-            {/* <WorkExperience /> */}
-            {/* <Projects /> */}
-            {/* <TechIUse /> */}
-            {/* <Connect /> */}
-            <Footer />
-          </div>
+          <Home />
+        </div>
       </motion.div>
     </ThemeProvider>
   )
---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import Intro from "../components/Intro"
+import Footer from "../components/Footer"
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <Intro />
+      <Footer />
+    </div>
+  )
+}
+
+export default Home
