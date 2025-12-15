82d604096775753d961c45f5559b7e90436c585a
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2025-12-16 01:15:26 +0530

    Bug: Contact page width issue

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar"
 const Contact = () => {
   return (
     <div className='h-screen flex flex-col w-full mx-auto px-6 pt-20 font-geist bg-white dark:bg-black text-black dark:text-white'>
-      <div className='h-full flex flex-col mx-auto max-w-2xl'>
+      <div className='h-full w-full flex flex-col mx-auto max-w-2xl'>
         <Navbar />
         <Connect />
       </div>
