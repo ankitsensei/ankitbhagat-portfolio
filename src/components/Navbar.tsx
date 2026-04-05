f5a5e019ec988ab4ceb3c39ed439bf4758bb2891
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-04-06 00:35:08 +0530

    style: Glass effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,17 @@ import DP from "../assets/ankit-dp.jpg"
 
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-center mb-10 mt-6 lg:mt-6 w-full text-sm text-zinc-300 font-geist">
-      <ul className="flex items-center gap-6 border border-zinc-700 rounded-lg w-fit px-6 py-2 bg-zinc-900">
+    <div className="sticky top-0 z-50 flex items-center justify-center mt-6 w-full text-sm text-zinc-300 font-geist">
+      <ul className="flex items-center gap-6 
+  rounded-xl px-6 py-2 
+  bg-white/10 backdrop-blur-lg 
+  border border-white/20 
+  shadow-lg shadow-black/30
+  supports-[backdrop-filter]:bg-white/10">
         <NavLink
           to="/"
           className={({ isActive }) =>
-            isActive ? "active" : ""
+            isActive ? "active text-white" : "text-zinc-400 hover:text-white transition"
           }
         >
           <img src={DP} alt="dp" className="w-10 rounded-lg" />
