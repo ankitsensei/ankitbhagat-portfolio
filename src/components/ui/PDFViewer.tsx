186fce29a5f45cd4263949f9ab2bb62e7febabb5
Ankit Bhagat <ankitbhagat.dev@gmail.com>
2026-05-29 23:19:58 +0530

    style: fixed light mode for resume page

--- src/components/ui/PDFViewer.tsx
+++ src/components/ui/PDFViewer.tsx
@@ -1,20 +1,26 @@
-import { createPluginRegistration } from '@embedpdf/core';
-import { EmbedPDF } from '@embedpdf/core/react';
-import { usePdfiumEngine } from '@embedpdf/engines/react';
-import { ExportPluginPackage } from '@embedpdf/plugin-export/react'
-import { useExport } from '@embedpdf/plugin-export/react';
+import { createPluginRegistration } from "@embedpdf/core";
+import { EmbedPDF } from "@embedpdf/core/react";
+import { usePdfiumEngine } from "@embedpdf/engines/react";
+import { ExportPluginPackage } from "@embedpdf/plugin-export/react";
+import { useExport } from "@embedpdf/plugin-export/react";
 
 // Icons
 import { LuDownload } from "react-icons/lu";
 
 // Import the essential plugins
-import { Viewport, ViewportPluginPackage } from '@embedpdf/plugin-viewport/react';
-import { Scroller, ScrollPluginPackage } from '@embedpdf/plugin-scroll/react';
+import {
+  Viewport,
+  ViewportPluginPackage,
+} from "@embedpdf/plugin-viewport/react";
+import { Scroller, ScrollPluginPackage } from "@embedpdf/plugin-scroll/react";
 import {
   DocumentContent,
   DocumentManagerPluginPackage,
-} from '@embedpdf/plugin-document-manager/react';
-import { RenderLayer, RenderPluginPackage } from '@embedpdf/plugin-render/react';
+} from "@embedpdf/plugin-document-manager/react";
+import {
+  RenderLayer,
+  RenderPluginPackage,
+} from "@embedpdf/plugin-render/react";
 
 import ResumePdf from "../../assets/ankit-resume.pdf";
 
@@ -28,58 +34,55 @@ const plugins = [
   createPluginRegistration(RenderPluginPackage),
   // Register the export plugin
   createPluginRegistration(ExportPluginPackage, {
-    defaultFileName: 'ankitbhagat-resume.pdf',
+    defaultFileName: "ankitbhagat-resume.pdf",
   }),
 ];
 
 const ExportToolbar = ({ documentId }: { documentId: string }) => {
-  const { provides: exportApi } = useExport(documentId)
+  const { provides: exportApi } = useExport(documentId);
   return (
-    <div className="w-full h-fit px-4 flex justify-between items-center dark:bg-zinc-950">
+    <div className="w-full h-fit px-4 flex justify-between items-center bg-white dark:bg-zinc-950">
       <p></p>
       <button
         onClick={() => exportApi?.download()}
         disabled={!exportApi}
-        className="inline-flex items-center gap-2 rounded-md px-3 py-1.5 text-sm font-medium text-white shadow-sm transition-all hover:bg-zinc-600 disabled:cursor-not-allowed disabled:opacity-50"
+        className="inline-flex items-center gap-2 rounded-md px-3 py-1.5 text-sm font-medium text-black dark:text-white shadow-sm transition-all hover:bg-zinc-200 dark:hover:bg-zinc-600 disabled:cursor-not-allowed disabled:opacity-50"
       >
         <LuDownload />
       </button>
     </div>
-  )
-}
+  );
+};
 
 export const PDFViewer = () => {
   // 2. Initialize the engine with the React hook
   const { engine, isLoading } = usePdfiumEngine();
 
   if (isLoading || !engine) {
-    return <div className='bg-black dark:bg-black'>Loading PDF Engine...</div>;
+    return <div className="bg-black dark:bg-black">Loading PDF Engine...</div>;
   }
 
   // 3. Wrap your UI with the <EmbedPDF> provider
   return (
-    <div className='w-full flex flex-col justify-center items-center bg-zinc-950 dark:bg-zinc-950'>
+    <div className="w-full flex flex-col justify-center items-center bg-white dark:bg-zinc-950">
       <EmbedPDF engine={engine} plugins={plugins}>
         {({ activeDocumentId }) =>
           activeDocumentId && (
             <DocumentContent documentId={activeDocumentId}>
               {({ isLoaded }) =>
                 isLoaded && (
-
-                  <Viewport
-                    documentId={activeDocumentId}
-                  >
+                  <Viewport documentId={activeDocumentId}>
                     <ExportToolbar documentId={activeDocumentId} />
                     <Scroller
-                      className='w-full'
+                      className="w-full"
                       documentId={activeDocumentId}
                       renderPage={({ pageIndex }) => (
-                        <div className='w-100 md:w-full h-fit flex flex-col items-center justify-center'>
+                        <div className="w-100 md:w-full h-fit flex flex-col items-center justify-center">
                           {/* The RenderLayer is responsible for drawing the page */}
                           <RenderLayer
                             documentId={activeDocumentId}
                             pageIndex={pageIndex}
-                            className='bg-zinc-950 border-2 rounded-2xl'
+                            className="bg-zinc-950 border-2 rounded-2xl"
                           />
                         </div>
                       )}
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,7 +4,7 @@ import { motion } from "motion/react";
 
 const Resume = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full text-sm bg-zinc-9 font-geist bg-zinc-950 dark:bg-zinc-950 text-black dark:text-white overflow-hidden">
+    <div className="flex flex-col items-center justify-center w-full text-sm bg-zinc-9 font-geist bg-white dark:bg-zinc-950 text-black dark:text-white overflow-hidden">
       <div className="w-full md:w-fit">
         <Navbar />
         <motion.div
