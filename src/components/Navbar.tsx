eb54fe516fe23642f1871b823ff357ec78db2de9
ankitsensei <ankitbhagat.dev@gmail.com>
2026-07-19 08:15:22 +0530

    style: navbar padding and theme

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,15 +15,8 @@ const Navbar = () => {
   const { theme, setTheme } = useTheme();
 
   return (
-    <div className="sticky top-4 md:top-10 z-50 flex items-center justify-center w-full text-sm text-zinc-300 font-geist mb-10 md:mb-16 lg:mb-16">
-      <ul
-        className="flex items-center gap-4 md:gap-6 
-  rounded-xl px-4 md:px-6 py-2 
-  bg-white/10 backdrop-blur-lg 
-  border border-white/20 
-  shadow-lg shadow-black/30
-  supports-[backdrop-filter]:bg-white/0"
-      >
+    <div className="sticky top-4 md:top-10 z-50 flex items-center justify-center w-full text-xs md:text-sm text-zinc-300 font-geist mb-10 md:mb-16 lg:mb-16">
+      <ul className="flex items-center gap-4 md:gap-4 rounded-xl px-3 md:px-4 py-2 bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg shadow-black/30 supports-[backdrop-filter]:bg-white/0">
         <NavLink
           to="/"
           className={({ isActive }) =>
@@ -44,8 +37,12 @@ const Navbar = () => {
         <a href="https://live-resume-preview-orcin.vercel.app/">Resume</a>
 
         <button
-          onClick={() => setTheme(themeOrder[(themeOrder.indexOf(theme) + 1) % themeOrder.length])}
-          className="ml-2 pl-4 border-l border-white/20 text-zinc-400 hover:text-white transition"
+          onClick={() =>
+            setTheme(
+              themeOrder[(themeOrder.indexOf(theme) + 1) % themeOrder.length],
+            )
+          }
+          className="w-6 px-5 border-l border-white/20 text-zinc-400"
         >
           <FontAwesomeIcon icon={themeIcon[theme]} />
         </button>
