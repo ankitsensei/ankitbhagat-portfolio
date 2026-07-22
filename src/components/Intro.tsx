bb79e6f8a7959ebfe174d9f4e2b06b048d2d65fe
ankitsensei <ankitbhagat.dev@gmail.com>
2026-07-22 21:22:20 +0530

    chore: linkedin url updated

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -53,10 +53,7 @@ const Intro: React.FC = () => {
             <a href="https://github.com/ankitsensei" target="_blank">
               <FaGithub />
             </a>
-            <a
-              href="https://www.linkedin.com/in/ankit-bhagat-139276241/"
-              target="_blank"
-            >
+            <a href="https://www.linkedin.com/in/ankitsensei/" target="_blank">
               <BsLinkedin />
             </a>
             {/* <a href="https://leetcode.com/u/ankitsensei/" target="_blank"><SiLeetcode /></a> */}
