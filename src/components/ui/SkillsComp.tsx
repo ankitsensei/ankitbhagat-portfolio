b772b065cf08e6fed36015ce1f41442006d48178
ankitsensei <ankitbhagat.dev@gmail.com>
2026-08-15 22:23:26 +0530

    style: text-color

--- src/components/ui/SkillsComp.tsx
+++ src/components/ui/SkillsComp.tsx
@@ -5,7 +5,7 @@ type SkillsCompProps = {
 
 const SkillsComp: React.FC<SkillsCompProps> = ({ title, icon }) => {
   return (
-    <div className="flex items-center justify-center gap-1 md:gap-2 border border-zinc-700 rounded-full w-fit px-1.5 md:px-[6px] py-[1px] text-xs md:text-sm dark:text-zinc-400 text-zinc-600">
+    <div className="flex items-center justify-center gap-1 md:gap-2 border border-zinc-700 rounded-full w-fit px-1.5 md:px-[6px] py-[1px] text-xs md:text-sm dark:text-zinc-600 text-zinc-600">
       <span className="text-sm md:text-base">{icon}</span>
       <span>{title}</span>
     </div>
